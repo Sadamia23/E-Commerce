820b3f657cce212809474a4d4798b0f11ed571eb
Sadamia23 <saba777adamia@gmail.com>
2025-04-18 01:58:24 +0400

    routing added

--- client/src/app/core/services/shop.service.ts
+++ client/src/app/core/services/shop.service.ts
@@ -39,6 +39,11 @@ export class ShopService {
       params,
     });
   }
+
+  getProduct(id: number) {
+    return this.http.get<Product>(this.baseUrl + 'products/' + id);
+  }
+
   getBrands() {
     if (this.brands.length > 0) return;
     return this.http.get<string[]>(this.baseUrl + 'products/brands').subscribe({
