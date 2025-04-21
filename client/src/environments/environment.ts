85239731dc27c4ad070b77b6e9a10abfca13e8b6
Sadamia23 <saba777adamia@gmail.com>
2025-04-22 01:25:05 +0400

    Pre continuoes integration

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: true,
     apiUrl: "api/",
     hubUrl: "hub/notifications",
-    stripePublicKey: "pk_test_51RFsdc4Qy3S2hT86ZBmKJnAOjPzPXekqyy3erzG7k48jZKUwzzrbNWjz7nfGwJuVozrsR6Ra2DCz3IyUww8YdFgJ00zg885R6J"
+    stripePublicKey: "pk_test_51RGQr3ECNynGM44BZpAxRr9ZUlRvhG42GeSyFGjQuqmA5TpvZcxAR1vPkdTLkaCzQ4Jyi6kJc8EIpEiaBepCLXsP0094LEbgov"
 };
